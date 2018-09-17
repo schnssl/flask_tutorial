e608069eea02462165a6d746a07c3246248dfc3e
schnssl <johannschnessl@gmail.com>
2018-09-17 17:19:46 +0200

    added logic to check user logged in before each request

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,6 +10,19 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?',
+            (user_id,)
+        ).fetchone()
+        
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
