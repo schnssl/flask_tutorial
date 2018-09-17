d13aab374c961a2edc025aea4748f76944eaabc8
schnssl <johannschnessl@gmail.com>
2018-09-17 16:55:53 +0200

    called db app init function from app factory

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 
 def create_app(test_config=None):
     # Create and configure the app
@@ -22,6 +23,8 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    db.init_app(app)
 
     # A simple page that says hello
     @app.route('/hello')
